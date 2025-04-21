e48e2fc6ebae73b9f51c12627ab27140c69806b7
Sorkand <96412234+sorkand1@users.noreply.github.com>
2025-04-21 19:54:13 +0300

    Create rpc-config.js

---
+++ celestia/testnet/js/rpc-config.js
@@ -0,0 +1,11 @@
+export const rpcServers = [
+    "https://celestia-testnet-rpc.itrocket.net",
+    "https://celestia-testnet.rpc.kjnodes.com",
+    "https://celestia-testnet-rpc.stake-town.com",
+    "https://celestia-testnet-rpc.stakeandrelax.net",
+    "https://rpc-t.celestia.nodestake.top",
+    "https://rpc-1.testnet.celestia.nodes.guru",
+    "https://rpc-2.testnet.celestia.nodes.guru"
+];
+
+console.log("Loaded RPC Servers:", rpcServers);
