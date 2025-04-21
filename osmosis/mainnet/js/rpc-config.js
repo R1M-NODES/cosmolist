4967a528afa7731d4e2c186da919c098e09279d5
Sorkand <96412234+sorkand1@users.noreply.github.com>
2025-04-21 19:59:14 +0300

    Create rpc-config.js

---
+++ osmosis/mainnet/js/rpc-config.js
@@ -0,0 +1,22 @@
+export const rpcServers = [
+    "https://osmosis-rpc.publicnode.com",           
+    "https://osmosis-rpc.kjnodes.com",              
+    "https://osmosis-rpc.polkachu.com",             
+    "https://osmosis-rpc.stakely.io",              
+    "https://osmosis.drpc.org",                     
+    "https://osmosis-rpc.chainroot.io",             
+    "https://rpc.osmosis.zone",                   
+    "https://osmosis-rpc.publicnode.com",        
+    "https://osmosis.drpc.org",                  
+    "https://1rpc.io/osmosis-rpc",                
+    "https://osmosis-rpc.chainroot.io",          
+    "https://osmosis-rpc.polkachu.com",           
+    "https://osmosis-rpc.noders.services",       
+    "https://rpc.lavenderfive.com/osmosis",      
+    "https://rpc-osmosis.ecostake.com",           
+    "https://osmosis-rpc.stake-town.com",        
+    "https://rpc.osmosis.bronbro.io",            
+    "https://1rpc.io/osmosis-rpc"    
+];
+
+console.log("Loaded RPC Servers:", rpcServers);
