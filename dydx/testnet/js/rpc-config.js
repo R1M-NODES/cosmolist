78f69485bc86e34e27963166659f99863172e2da
Sorkand <96412234+sorkand1@users.noreply.github.com>
2025-04-21 20:07:10 +0300

    Create rpc-config.js

---
+++ dydx/testnet/js/rpc-config.js
@@ -0,0 +1,6 @@
+export const rpcServers = [
+"https://dydx-testnet-rpc.polkachu.com:443",      
+"https://endpoints.omniatech.io/v1/dydx/testnet/public"
+];
+
+console.log("Loaded RPC Servers:", rpcServers);
