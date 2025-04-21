d5a07e9bbd5834173e05309b2ad38c089ea16d61
Sorkand <96412234+sorkand1@users.noreply.github.com>
2025-04-21 19:57:30 +0300

    Create rpc-config.js

---
+++ cosmos/testnet/js/rpc-config.js
@@ -0,0 +1,9 @@
+export const rpcServers = [
+    "https://cosmos-testnet-rpc.itrocket.net", 
+    "https://cosmoshub-testnet.rpc.kjnodes.com", 
+    "https://rpc.provider-sentry-01.ics-testnet.polypore.xyz",
+    "https://rpc.provider-sentry-02.ics-testnet.polypore.xyz", 
+    "https://rpc-rs.cosmos.nodestake.top"
+];
+
+console.log("Loaded RPC Servers:", rpcServers);
