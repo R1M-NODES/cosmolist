3b04df5bc21aa3266130f532ba05ce09a3a798a5
Sorkand <96412234+sorkand1@users.noreply.github.com>
2025-04-21 19:52:48 +0300

    Create rpc-config.js

---
+++ celestia/mainnet/js/rpc-config.js
@@ -0,0 +1,11 @@
+export const rpcServers = [
+    "https://celestia-rpc.stake-town.com",
+    "https://celestia-mainnet-rpc.itrocket.net",
+    "https://celestia.rpc.kjnodes.com",
+    "https://celestia-rpc.noders.services",
+    "https://rpc.lunaroasis.net",
+    "https://public-celestia-rpc.numia.xyz"
+];
+
+
+console.log("Loaded RPC Servers:", rpcServers);
