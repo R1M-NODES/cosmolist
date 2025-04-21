0779e0429496cd3b3a1bd85fc75e91126c67b17d
Sorkand <96412234+sorkand1@users.noreply.github.com>
2025-04-21 20:00:17 +0300

    Create rpc-config.js

---
+++ osmosis/testnet/js/rpc-config.js
@@ -0,0 +1,8 @@
+export const rpcServers = [
+    "https://rpc.testnet.osmosis.zone",
+    "https://rpc.osmotest5.osmosis.zone",
+    "https://osmosis-testnet-rpc.polkachu.com",
+    "https://osmosis-testnet.drpc.org"
+];
+
+console.log("Loaded RPC Servers:", rpcServers);
