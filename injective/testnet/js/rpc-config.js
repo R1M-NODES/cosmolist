1c8ab1fb8e9e5494e52a5f9f9c75cf6e61966c77
Sorkand <96412234+sorkand1@users.noreply.github.com>
2025-04-21 20:04:16 +0300

    Create rpc-config.js

---
+++ injective/testnet/js/rpc-config.js
@@ -0,0 +1,10 @@
+export const rpcServers = [
+"https://testnet.sentry.tm.injective.network:443",
+"https://injective-testnet-rpc.publicnode.com", 
+"https://injective-testnet-rpc.polkachu.com" 
+
+];
+
+
+console.log("Loaded RPC Servers:", rpcServers);
+
