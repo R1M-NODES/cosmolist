af45b43e1f9c4564fb17139f81d8ff1523e45b7b
Sorkand <96412234+sorkand1@users.noreply.github.com>
2025-04-21 19:56:11 +0300

    Create rpc-config.js

---
+++ cosmos/mainnet/js/rpc-config.js
@@ -0,0 +1,15 @@
+export const rpcServers = [
+    "https://cosmoshub-mainnet-rpc.itrocket.net",
+    "https://cosmoshub.rpc.kjnodes.com",
+    "https://cosmos-rpc.publicnode.com",       
+    "https://cosmos-hub.drpc.org",             
+    "https://cosmos.blockpi.network/rpc/v1/public", 
+    "https://api.flare.network/cosmos/rpc",    
+    "https://go.getblock.io/55aae677e66541c4a47741aa201a4b08", 
+    "https://endpoints.omniatech.io/v1/cosmos/cosmoshub-4/public",
+    "https://cosmoshub.rpc.stakin-nodes.com",  
+    "https://cosmoshub.tendermintrpc.lava.build" 
+];
+
+
+console.log("Loaded RPC Servers:", rpcServers);
